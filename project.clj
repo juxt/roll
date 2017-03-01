e944bd2739e637231ce17edbce6ed98e9ab5b873
Jon Pither <jon.pither@gmail.com>
2017-03-01 11:26:14 +0000

    Adding project.clj

---
+++ project.clj
@@ -0,0 +1,9 @@
+;; Copyright © 2017, JUXT LTD.
+
+(defproject roll "0.0.4"
+  :description "Rolling Terraform releases orchestrated with ClojureScript."
+  :url "http://github.com/juxt/roll"
+  :license {:name "The MIT License"
+            :url "http://opensource.org/licenses/MIT"}
+  :dependencies []
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.8.0"]]}})

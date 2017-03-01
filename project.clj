1b3cb9a0900aac3ff777f1f1438d9efb890f7d2b
Jon Pither <jon.pither@gmail.com>
2017-03-01 16:39:30 +0000

    Adding Sha

--- project.clj
+++ project.clj
@@ -5,5 +5,9 @@
   :url "http://github.com/juxt/roll"
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies []
-  :profiles {:dev {:dependencies [[org.clojure/clojure "1.8.0"]]}})
+  :resource-paths ["tf"]
+  :source-paths ["src"]
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.8.0"]]}}
+  :plugins [[org.clojars.ride_on/lein-sha-version "0.3.0"]]
+  :sha {:manifest-header "Git-Sha"
+        :write-version? false})

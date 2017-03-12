1f1f21d3671d7c45555744b62194d9c4ee3389ff
Jon Pither <jon.pither@gmail.com>
2017-03-12 12:27:08 +0000

    Updates for usage

---
+++ src/roll/tf.cljs
@@ -0,0 +1,22 @@
+(ns roll.tf
+  (:require [cljs.reader :as reader]
+            [cljs.nodejs :as nodejs]
+            [roll.core]))
+
+(def fs (nodejs/require "fs"))
+
+(defn- write-tf [data output-file]
+  (let [output-file (or output-file "deployment.tf.json")]
+    (println "Reading" output-file)
+    (fs.writeFileSync output-file data)))
+
+(defn -main [role-file output-file]
+  (let [role-file (or role-file "roll.edn")]
+    (println "Reading" role-file)
+    (-> role-file
+        (fs.readFileSync "utf-8")
+        reader/read-string
+        roll.core/preprocess
+        roll.core/deployment->tf
+        roll.core/->tf-json
+        (write-tf output-file))))
--- test/roll/lumo_test.cljs
+++ test/roll/lumo_test.cljs
@@ -1,7 +1,14 @@
 (ns roll.lumo-test
   (:require [roll.core-test]
-            [cljs.test :refer-macros [deftest is testing run-tests]]))
+            [cljs.reader :as reader]
+            [cljs.test :refer-macros [deftest is testing run-tests]]
+            [cljs.nodejs :as nodejs]))
+
+(def fs (nodejs/require "fs"))
 
 (defn -main [& argv]
   (println "Testing with lumo")
+
+  (is (= (reader/read-string (fs.readFileSync "sample-config.edn" "utf-8"))))
+
   (run-tests 'roll.core-test))
